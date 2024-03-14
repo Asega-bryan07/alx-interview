ce39859de72a29bdad933db5fad56dea9c18a424
Asega-bryan07 <Almasibryan7@gmail.com>
2024-03-14 23:12:21 +0300

    Interview

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,70 @@
+#!/bin/bash/python3
+
+'''
+You have n number of locked boxes in front of you. Each box
+is numbered sequentially from 0 to n - 1 and each box may contain
+keys to the other boxes.
+Write a method that determines if all the boxes can be opened.
+Prototype: def canUnlockAll(boxes)
+boxes is a list of lists
+A key with the same number as a box opens that box
+You can assume all keys will be positive integers
+There can be keys that do not have boxes
+The first box boxes[0] is unlocked
+Return True if all boxes can be opened, else return False
+'''
+
+def look_next_opened_box(opened_boxes):
+    '''looks for the next opened box
+    Args:
+    opened_boxes (dict): Dictionary which contains boxes already opened
+    Returns:
+    list: List with the keys contained in the opened box
+    '''
+    for index, box in opened_boxes.items():
+        if box.get('status') == 'opened':
+            box['status'] = 'opened/checked':
+                return box.get('keys')
+    return None
+
+def canUnlockAll(boxes):
+    if len(boxes) <= 1 or boxes == [[]]:
+        return True
+
+    aux = {}
+    while True:
+        if len(aux) == 0:
+            aux[0] = {
+                    'status': 'opened',
+                    'keys': boxes[0],
+                    }
+            keys = look_next_opened_box(aux)
+        if keys:
+            for key in keys:
+                try:
+                    if aux.get(key) and aux.get(key).get('status') \
+                            == 'opened/checked':
+                                continue
+                    aux[key] = {
+                            'status': 'opened',
+                            'keys': boxes[0],
+                            }
+                except (KeyError, IndexError):
+                    continue
+        elif 'opened' in [box.get('status') for box in aux.values()]:
+            continue
+        elif len(aux) == len(boxes):
+            break
+        else:
+            return False
+
+    return len(aux) == len(boxes)
+
+
+def main():
+    '''Entry Point'''
+    canUnlockAll([[]])
+
+
+if __name__ == "__main__":
+    main()
